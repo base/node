8cee8bd106671c9a39132fff20b59774493f4413
JenabaBa <dieynaba.ba@coinbase.com>
2025-06-17 12:13:25 -0700

    dependency updater script updates

--- dependency_updater/dependency_updater.go
+++ dependency_updater/dependency_updater.go
@@ -3,27 +3,23 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"strings"
 	"log"
 	"context"
 	"github.com/urfave/cli/v3"
+	"github.com/google/go-github/v72/github"
 )
 
 type Info struct {
-	Repo string `json:"repo"`
+	RepoUrl string `json:"repoUrl"`
 	Tag string `json:"tag"`
 	Commit string `json:"commit"`
-}
-
-type Dependencies struct {
-	Op_Node Info `json:"op_node"`
-	Op_Geth Info `json:"op_geth"`
-	Op_Reth Info `json:"op_reth"`
-	Nethermind Info `json:"nethermind"`
-	Base_Reth_Node Info `json:"base_reth_node"`
+	CommitUrl string `json:"commitUrl"`
+	VersionUrl string `json:"versionUrl"`
+	TagPrefix *string `json:"tagPrefix,omitempty"`
+	Owner string `json:"owner`
+	Repo string `json:"repo`
 }
 
 type VersionTag []struct {
@@ -34,12 +30,28 @@ type Commit struct {
 	Commit string `json:"sha"`
 }
 
+type Dependencies = map[string]*Info
+
 func main() {
 	cmd := &cli.Command {
 		Name: "updater",
 		Usage: "Updates the dependencies in the geth, nethermind and reth Dockerfiles",
-		Action: func(context.Context, *cli.Command) error {
-			err := updater()
+		Flags: 
+		[]cli.Flag{
+			&cli.StringFlag {
+				Name: "token",
+				Usage: "Auth token used to make requests to the Github API must be set using export",
+				Sources: cli.EnvVars("GITHUB_TOKEN"),
+				Required: true,
+			},
+			&cli.StringFlag {
+				Name: "repo",
+				Usage: "Specifies repo location to run the version updater on",
+				Required: true,
+			},
+		},
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			err := updater(string(cmd.String("token")), string(cmd.String("repo")))
 			if err != nil {
 				return fmt.Errorf("error running updater: %s", err)
 			}
@@ -52,10 +64,10 @@ func main() {
 	}
 }
 
-func updater() error {
+func updater(token string, repoPath string) error {
 	var err error
 
-	f, err := os.ReadFile("../versions.json")
+	f, err := os.ReadFile(repoPath + "/versions.json")
 	if err != nil {
 		return fmt.Errorf("error reading versions JSON: %s", err)
 	}
@@ -67,68 +79,19 @@ func updater() error {
 		return fmt.Errorf("error unmarshaling versions JSON to dependencies: %s", err)
 	}
 
-	token := os.Getenv("GITHUB_TOKEN")
-
-	// updating op reth version
-	err = get_and_update_version_commit(
-		"https://api.github.com/repos/paradigmxyz/reth/commits/"+dependencies.Op_Reth.Tag,
-		"https://api.github.com/repos/paradigmxyz/reth/releases",
-		"op_reth",
-		token,
-		&dependencies,
-	)
-	if err != nil {
-		return fmt.Errorf("error getting and updating version/commit for reth: %s", err)
+	for dependency := range dependencies {
+		err = getAndUpdateDependency(
+			dependency,
+			token,
+			repoPath,
+			dependencies,
+		)
+		if err != nil {
+			return fmt.Errorf("error getting and updating version/commit for " + dependency + ": %s", err)
+		}
 	}
 
-	// updating op geth version
-	err = get_and_update_version_commit(
-		"https://api.github.com/repos/ethereum-optimism/op-geth/commits/"+dependencies.Op_Geth.Tag,
-		"https://api.github.com/repos/ethereum-optimism/op-geth/releases",
-		"op_geth",
-		token,
-		&dependencies,
-	)
-	if err != nil {
-		return fmt.Errorf("error getting and updating version/commit for op geth: %s", err)
-	}
-
-	// updating op node version
-	err = get_and_update_version_commit(
-		"https://api.github.com/repos/ethereum-optimism/optimism/commits/"+dependencies.Op_Node.Tag,
-		"https://api.github.com/repos/ethereum-optimism/optimism/releases",
-		"op_node",
-		token,
-		&dependencies,
-	)
-	if err != nil {
-		return fmt.Errorf("error getting and updating version/commit for op node: %s", err)
-	}
-	// updating nethermind version
-	err = get_and_update_version_commit(
-		"https://api.github.com/repos/NethermindEth/nethermind/commits/"+dependencies.Nethermind.Tag,
-		"https://api.github.com/repos/NethermindEth/nethermind/releases",
-		"nethermind",
-		token,
-		&dependencies,
-	)
-	if err != nil {
-		return fmt.Errorf("error getting and updating version/commit for nethermind: %s", err)
-	}
-
-	// updating base reth node version
-	err = get_and_update_version_commit(
-		"https://api.github.com/repos/base/node-reth/commits/"+dependencies.Base_Reth_Node.Tag,
-		"https://api.github.com/repos/base/node-reth/releases",
-		"base_reth_node",
-		token,
-		&dependencies,
-	)
-	if err != nil {
-		return fmt.Errorf("error getting and updating version/commit for base reth node: %s", err)
-	}
-
-	e := create_versions_env(dependencies)
+	e := createVersionsEnv(repoPath, dependencies)
 	if e != nil {
 		return fmt.Errorf("error creating versions.env: %s", e)
 	}
@@ -136,23 +99,14 @@ func updater() error {
 	return nil
 }
 
-func get_and_update_version_commit(
-	commitUrl string,
-	versionUrl string,
-	dependency_type,
+func getAndUpdateDependency(
+	dependencyType string,
 	token string,
-	dependencies *Dependencies) error {
-	tag, err := get_version_tag(versionUrl, token)
-	if err != nil {
-		return fmt.Errorf("error getting "+dependency_type, " version tag: %s", err)
-	}
+	repoPath string,
+	dependencies Dependencies) error {
+	version, commit, _ := getVersionAndCommit(token, dependencies, dependencyType)
 
-	commit, err := get_commit(commitUrl, token)
-	if err != nil {
-		return fmt.Errorf("error getting "+dependency_type, " commit: %s", err)
-	}
-
-	e := update_version_tag_and_commit(commit, tag, dependency_type, dependencies)
+	e := updateVersionTagAndCommit(commit, version, dependencyType, repoPath, dependencies)
 	if e != nil {
 		return fmt.Errorf("error updating version tag and commit: %s", e)
 	}
@@ -160,140 +114,76 @@ func get_and_update_version_commit(
 	return nil
 }
 
-func get_version_tag(url string, token string) (string, error) {
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return "", fmt.Errorf("error creating new GET request (version tag):  %s", err)
-	}
+func getVersionAndCommit(token string, dependencies Dependencies, dependencyType string) (string, string, error){
+	client := github.NewClient(nil).WithAuthToken(token)
+	ctx := context.Background()
 
-	req.Header.Set("Accept", "application/vnd.github+json")
-	req.Header.Set("Authorization", "Bearer "+token)
-	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
+	var version *github.RepositoryRelease
+	var err error
+	// handle dependencies with prefix
+	if dependencies[dependencyType].TagPrefix != nil {
+		releases, _, err := client.Repositories.ListReleases(
+			ctx, 
+			dependencies[dependencyType].Owner,
+			dependencies[dependencyType].Repo,
+			nil)
 
-	jsonBody, err := get_json_body(req)
-	if err != nil {
-		return "", fmt.Errorf("error getting json body:  %s", err)
-	}
-
-	var versionTag VersionTag
-
-	e := json.Unmarshal(jsonBody, &versionTag)
-	if e != nil {
-		return "", fmt.Errorf("error unmarshaling:  %s", e)
-	}
-
-	// loop to return most recent version of op-node
-	if url == "https://api.github.com/repos/ethereum-optimism/optimism/releases" {
-		incrementor := 0
-		for !(strings.HasPrefix(versionTag[incrementor].Tag, "op-node")) {
-			incrementor += 1
+		if err != nil {
+			return "", "", fmt.Errorf("error getting releases: %s", err)
 		}
-		return versionTag[incrementor].Tag, nil
+
+		for release := range releases {
+			if strings.HasPrefix(*releases[release].TagName, *dependencies[dependencyType].TagPrefix){
+				version = releases[release]
+				break
+			}
+		}
+	} else {
+		release, _, err := client.Repositories.GetLatestRelease(
+			ctx, 
+			dependencies[dependencyType].Owner,
+			dependencies[dependencyType].Repo)
+		if err != nil {
+			return "", "", fmt.Errorf("error getting releases: %s", err)
+		}
+		version = release
 	}
 
-	return versionTag[0].Tag, nil
+	commit, _, err := client.Git.GetRef(
+		ctx, 
+		dependencies[dependencyType].Owner,
+		dependencies[dependencyType].Repo, 
+		"refs/tags/"+*version.TagName)
+	if err != nil {
+		return "", "", fmt.Errorf("error getting commit for "+dependencyType + ": %s", err)
+	}
+
+	return *version.TagName, *commit.Object.SHA, nil
 }
 
-func get_commit(url string, token string) (string, error) {
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return "", fmt.Errorf("error creating new GET request (commit): %s", err)
-	}
-
-	req.Header.Set("Accept", "application/vnd.github+json")
-	req.Header.Set("Authorization", "Bearer "+token)
-	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
-
-	jsonBody, err := get_json_body(req)
-	if err != nil {
-		return "", fmt.Errorf("error getting json body:  %s", err)
-	}
-
-	var commit Commit
-
-	e := json.Unmarshal(jsonBody, &commit)
-	if e != nil {
-		fmt.Println("Error unmarshaling")
-		return "", fmt.Errorf("error unmarshaling:  %s", e)
-	}
-
-	return string(commit.Commit), nil
-}
-
-func get_json_body(req *http.Request) ([]byte, error) {
-	client := &http.Client{}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("error making GET request to client:  %s", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("received http status code when getting json body:  %s", resp.Status)
-	}
-
-	jsonBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("error reading json body:  %s", err)
-	}
-
-	return jsonBody, nil
-}
-
-func update_version_tag_and_commit(
+func updateVersionTagAndCommit(
 	commit string, 
 	tag string, 
-	dependency_type string, 
-	dependencies *Dependencies) error {
-	if dependency_type == "op_reth" {
-		dependencies.Op_Reth.Tag = tag
-		dependencies.Op_Reth.Commit = commit
-		err := write_to_versions_env(*dependencies)
-		if err != nil {
-			return fmt.Errorf("error writing to versions (op reth): %s", err)
-		}
-	} else if dependency_type == "op_node" {
-		dependencies.Op_Node.Tag = tag
-		dependencies.Op_Node.Commit = commit
-		err := write_to_versions_env(*dependencies)
-		if err != nil {
-			return fmt.Errorf("error writing to versions (op node): %s", err)
-		}
-	} else if dependency_type == "op_geth" {
-		dependencies.Op_Geth.Tag = tag
-		dependencies.Op_Geth.Commit = commit
-		err := write_to_versions_env(*dependencies)
-		if err != nil {
-			return fmt.Errorf("error writing to versions (op geth): %s", err)
-		}
-	} else if dependency_type == "nethermind" {
-		dependencies.Nethermind.Tag = tag
-		dependencies.Nethermind.Commit = commit
-		err := write_to_versions_env(*dependencies)
-		if err != nil {
-			return fmt.Errorf("error writing to versions (nethermind): %s", err)
-		}
-	} else if dependency_type == "base_reth_node" {
-		dependencies.Base_Reth_Node.Tag = tag
-		dependencies.Base_Reth_Node.Commit = commit
-		err := write_to_versions_env(*dependencies)
-		if err != nil {
-			return fmt.Errorf("error writing to versions (base reth node): %s", err)
-		}
+	dependencyType string,
+	repoPath string, 
+	dependencies Dependencies) error {
+	dependencies[dependencyType].Tag = tag
+	dependencies[dependencyType].Commit = commit
+	err := writeToVersionsEnv(repoPath, dependencies)
+	if err != nil {
+		return fmt.Errorf("error writing to versions " + dependencyType + ": %s", err)
 	}
-
 	return nil
 }
 
-func write_to_versions_env(dependencies Dependencies) error {
+func writeToVersionsEnv(repoPath string, dependencies Dependencies) error {
 	// formatting json
 	updatedJson, err := json.MarshalIndent(dependencies, "", "	  ")
 	if err != nil {
 		return fmt.Errorf("error Marshaling dependencies json: %s", err)
 	}
 
-	e := os.WriteFile("../versions.json", updatedJson, 0644)
+	e := os.WriteFile(repoPath + "/versions.json", updatedJson, 0644)
 	if e != nil {
 		return fmt.Errorf("error writing to versions.json: %s", e)
 	}
@@ -301,30 +191,29 @@ func write_to_versions_env(dependencies Dependencies) error {
 	return nil
 }
 
-func create_versions_env(dependencies Dependencies) error {
-	env := "export OP_NODE_TAG=" + dependencies.Op_Node.Tag + "\n" +
-		   "export OP_NODE_COMMIT=" + dependencies.Op_Node.Commit + "\n" +
-		   "export OP_NODE_REPO=https://github.com/ethereum-optimism/optimism.git" + "\n\n" +
-		   "export OP_GETH_TAG=" + dependencies.Op_Geth.Tag + "\n" +
-		   "export OP_GETH_COMMIT=" + dependencies.Op_Geth.Commit + "\n" +
-		   "export OP_GETH_REPO=https://github.com/ethereum-optimism/op-geth.git" + "\n\n" +
-		   "export OP_RETH_TAG=" + dependencies.Op_Reth.Tag + "\n" +
-		   "export OP_RETH_COMMIT=" + dependencies.Op_Reth.Commit + "\n" +
-		   "export OP_RETH_REPO=https://github.com/paradigmxyz/reth.git" + "\n\n" +
-		   "export NETHERMIND_TAG=" + dependencies.Nethermind.Tag + "\n" +
-		   "export NETHERMIND_COMMIT=" + dependencies.Nethermind.Commit + "\n" +
-		   "export NETHERMIND_REPO=https://github.com/NethermindEth/nethermind.git" + "\n\n" +
-		   "export BASE_RETH_NODE_TAG=" + dependencies.Base_Reth_Node.Tag + "\n" +
-		   "export BASE_RETH_NODE_COMMIT=" + dependencies.Base_Reth_Node.Commit + "\n" +
-		   "export BASE_RETH_NODE_REPO=https://github.com/base/node-reth.git " + "\n\n"
+func createVersionsEnv(repoPath string, dependencies Dependencies) error {
+	envLines := []string{}
 
-	file, err := os.Create("../versions.env")
+	for dependency := range dependencies {
+		dependencyPrefix := strings.ToUpper(dependency)
+
+		envLines = append(envLines, fmt.Sprintf("export %s_%s=%s \n", 
+		dependencyPrefix, "TAG", dependencies[dependency].Tag))
+
+		envLines = append(envLines, fmt.Sprintf("export %s_%s=%s \n",
+		dependencyPrefix, "COMMIT", dependencies[dependency].Commit))
+
+		envLines = append(envLines, fmt.Sprintf("export %s_%s=%s \n\n",
+		dependencyPrefix, "REPO", dependencies[dependency].RepoUrl))
+	}
+
+	file, err := os.Create(repoPath + "/versions.env")
 	if err != nil {
 		return fmt.Errorf("error creating versions.env file: %s", err)
 	}
 	defer file.Close()
 
-	_, err = file.WriteString(env)
+	_, err = file.WriteString(strings.Join(envLines, " "))
 	if err != nil {
 		return fmt.Errorf("error writing to versions.env file: %s", err)
 	}
--- dependency_updater/go.mod
+++ dependency_updater/go.mod
@@ -2,4 +2,9 @@ module dependency_updater
 
 go 1.24.3
 
-require github.com/urfave/cli/v3 v3.3.8 // indirect
+require (
+	github.com/google/go-github/v72 v72.0.0
+	github.com/urfave/cli/v3 v3.3.8
+)
+
+require github.com/google/go-querystring v1.1.0 // indirect
